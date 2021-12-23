1e19fc5cebb5f7ace0a2e23b0859b7ffe683891d
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-23 20:55:45 +0100

    feature: Finish day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use anyhow::Result;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Result for day 1 part 1 is: {:?}", day_1(2));
+    println!("Result for day 1 part 2 is: {:?}", day_1(4));
+}
+
+fn day_1(size: usize) -> Result<usize> {
+    let path = String::from("./data/1.input");
+    Ok(aoc::read_one_per_line::<u32>(&path)?
+        .windows(size)
+        .filter(|win| win[0] < win[size - 1])
+        .collect_vec()
+        .len())
 }
